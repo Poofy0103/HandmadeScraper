1aad231cfbddc246f5797016c107508ebd71e477
Thien Diep <thiendb0103@gmail.com>
2024-12-19 14:18:42 +0700

    Complete asynchronous scraping for product links

--- common/utils.py
+++ common/utils.py
@@ -1,37 +1,50 @@
 import asyncio
-from datamodel.model import ListTasks
 from typing import List
 import uuid
+import time
 import datetime
 
 class AsyncioManager:
-    def __init__(self, maxSize = 3):
-        self.queue = asyncio.Queue(maxSize)
+    def __init__(self, maxSize = 0, queuesNum = 2):
+        self.maxSize = maxSize
         self.completedTasks = 0
         self.runningTasks = 0
-        self.pendingTasks = 0
+        self.queuesNum = queuesNum
+        self.queues: List[asyncio.Queue] = []
         
-    
     async def add_task(self, coro):
-        self.pendingTasks += 1
-        await self.queue.put(coro)
-        self.pendingTasks -= 1
         self.runningTasks += 1
-        print(f"[{str(datetime.datetime.now())}] Added a new task")
+        try:
+            tasksInQueues = [tasks.qsize() for tasks in self.queues]
+            print(tasksInQueues)
+            idx_max = tasksInQueues.index(min(tasksInQueues))
+            await self.queues[idx_max].put(coro)
+            print(f"[{str(datetime.datetime.now())}] Added a new task")
+        except:
+            print("Please add queues to the list")
+    
+    def create_queue(self):
+        for i in range(self.queuesNum):
+            self.queues.append(asyncio.Queue(self.maxSize))
         
-    async def execute_task(self):
+    async def execute_task(self, queue: asyncio.Queue, index):
+        print(f"Execute the queue {index}")
         while True:
-            coro = await self.queue.get()
+            coro = await queue.get()
             try:
                 await coro
-                self.runningTasks -= 1
-                self.completedTasks += 1
             except Exception as e:
                 print("Failed to execute this task")
-            self.queue.task_done()
+            self.runningTasks -= 1
+            self.completedTasks += 1
+            queue.task_done()
     
-    async def monitor_task(self, interval: int = 3):
+    async def start_all_queues(self):
+        tasks = [self.execute_task(queue, index) for index, queue in enumerate(self.queues)]
+        await asyncio.gather(*tasks)
+    
+    def monitor_task(self, interval: int = 3):
         while True:
             print(f"[{str(datetime.datetime.now())}] {self.runningTasks} task(s) running. Completed: {self.completedTasks}")
-            await asyncio.sleep(interval)
+            time.sleep(interval)
     
--- scraper/scraper_factory.py
+++ scraper/scraper_factory.py
@@ -1,7 +1,7 @@
 from .amazon_scraper_class import AmazonPWrightScraper
 
 class ScraperFactory:
-    def get_scraper(self, platform, homepage=None, proxy=None, hasSignIn=False, productNames=["Gaming Chair", "Gaming Headset"],account={"username": "diepbaothien10x@gmail.com", "password": "thien1"}, headless=False):
+    def get_scraper(self, platform, homepage=None, proxy=None, hasSignIn=False, productNames=["Gaming Chair", "Gaming Headset"],account={"username": "diepbaothien10x@gmail.com", "password": "thien1"}, headless=False, maxQueueSize = 0, queuesNum = 2):
         if platform == "amazon":
             print("Retrieve Amazon Configuration")
             return AmazonPWrightScraper(homepage="https://amazon.com" if homepage == None else homepage
@@ -9,7 +9,9 @@ class ScraperFactory:
                                         , productNames=productNames
                                         , account=account
                                         , headless=headless
-                                        , proxy=proxy)
+                                        , proxy=proxy
+                                        , maxQueueSize=maxQueueSize
+                                        , queuesNum=queuesNum)
         # elif platform == "ebay":
         #     return EbayScraper()
         else:
