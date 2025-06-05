1d991f11bbdc667ed1ea4c5a1901c3516a6e98af
Thien Diep <thiendb0103@gmail.com>
2025-06-05 22:38:47 +0700

    Remove redundant code

--- scraper/scraper_factory.py
+++ scraper/scraper_factory.py
@@ -2,10 +2,10 @@
 from .finance_scraper_class import FinanceScraperClass
 from .news_scraper_class import NewsScraperClass
 from .macro_metrics_scraper_class import MacroMetricsScraperClass
-from .travelling_scraper_class_copy import TravellingScraperClass
+from .travelling_scraper_class import TravellingScraperClass
 
 class ScraperFactory:
-    def get_scraper(self, platform, homepage=None, proxy=None, hasSignIn=False, stockCodes=["Gaming Chair", "Gaming Headset"],account={"username": "diepbaothien10x@gmail.com", "password": "thien1"}, headless=False, maxQueueSize = 0, queuesNum = 2, timeRange = '01-01-2021 - 31-12-2024'):
+    def get_scraper(self, platform, scroll_page_attempt = 5, homepage=None, proxy=None, hasSignIn=False, stockCodes=["Gaming Chair", "Gaming Headset"],account={"username": "diepbaothien10x@gmail.com", "password": "thien1"}, headless=False, maxQueueSize = 0, queuesNum = 2, timeRange = '01-01-2021 - 31-12-2024'):
         if platform == "finance":
             print("Retrieve Finance Scraper Configuration")
             return FinanceScraperClass(homepage="https://www.booking.com/searchresults.en-gb.html?label=en-vn-booking-desktop-WeZI9wwaGAAqHXeGoKbrHQS652828997899%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp9198864%3Ali%3Adec%3Adm&gclid=Cj0KCQjwgIXCBhDBARIsAELC9Zj8DwnfTSotcbQLWXSb_2k986ObVIOJw8qQzlzGxvp681nwUjPeAXEaAkIwEALw_wcB&aid=2311236&checkin=2025-06-07&checkout=2025-06-08&dest_id=-3730078&dest_type=city&group_adults=null&req_adults=null&no_rooms=null&group_children=null&req_children=null" if homepage == None else homepage
@@ -47,7 +47,8 @@ class ScraperFactory:
                                         , headless=headless
                                         , proxy=proxy
                                         , maxQueueSize=maxQueueSize
-                                        , queuesNum=queuesNum)
+                                        , queuesNum=queuesNum
+                                        , scroll_page_attempt=scroll_page_attempt)
         else:
             raise ValueError(f"No scraper available for {platform}")
         
--- scraper/travelling_scraper_class.py
+++ scraper/travelling_scraper_class.py
@@ -1,56 +1,85 @@
 from .web_scraper_class import WebsitePWrightScraper, PlaywrightBaseScraper
+from common.pyppeteer_utils import PyppeteerUtils
 from common.playwright_utils import PlaywrightUtils
-from playwright.async_api import Page, Locator, BrowserContext
+from playwright.async_api import Page, BrowserContext, Locator
 import asyncio
-# from common.asyncioManager import AsyncioManager
+from pyppeteer import launch, browser, page
+import uuid
+from common.asyncioManager import AsyncioManager
+# from common.utils.ggcloud import CloudManager
 import threading
 from bs4 import BeautifulSoup, Comment
 from tqdm.asyncio import tqdm
 import os
-import pandas as pd
+import json
+from common.config import read_config
+from rich.live import Live
+from rich.progress import Progress, BarColumn, TextColumn, TimeRemainingColumn, TaskProgressColumn
+import aiofiles
 import json
 
-class TravellingScraperClassCopy(WebsitePWrightScraper):
+
+
+class TravellingScraperClass(WebsitePWrightScraper):
     product_page = []
     isNextProduct = False
     condition = asyncio.Condition()
     # cloudManager = CloudManager()
+    # config_values = read_config()
 
-    def __init__(self, places: list, homepage, timeRange, hasSignIn = False, account=dict(username=None, password=None), headless=False, proxy=None, maxQueueSize = 0, queuesNum = 2):
+    def __init__(self, productNames: list, homepage, scroll_page_attempt: int, hasSignIn = False, account=dict(username=None, password=None), headless=False, proxy=None, maxQueueSize = 0, queuesNum = 2):
         super().__init__(headless, proxy)
         self.homepage = homepage
         self.account = account
-        self.places = places
+        self.productNames = productNames
         self.hasSignIn = hasSignIn
-        self.timeRange = timeRange
-        # self.asyncManager = AsyncioManager(maxSize=maxQueueSize, queuesNum=queuesNum)
+        self.scroll_page_attempt = scroll_page_attempt
+        self.asyncManager = AsyncioManager(maxSize=maxQueueSize, queuesNum=queuesNum)
 
     async def scraping_driver(self):
-        await self.initialize_browser()
-        workers = [TravellingScraperWorker(self.main_context, self.homepage, place, self.timeRange) for place in self.places]
-        tasks = [worker.scraping_task() for worker in workers]
-        await asyncio.gather(*tasks)
+        #Open another thread to monitor the task separately
+        progress = Progress(
+            TextColumn("[bold blue]{task.fields[label]}"),
+            BarColumn(),
+            TaskProgressColumn(),
+            TimeRemainingColumn()
+        )
+        with Live(progress, refresh_per_second=3) as live:
+            threading.Thread(target=self.asyncManager.monitor_task, args=(progress,), daemon=True).start()
+
+            await self.initialize_browser()
+            workers = [TravellingScraperWorker(self.main_context, self.homepage, place, self.asyncManager, self.scroll_page_attempt) for place in self.productNames]
+            tasks = [worker.scraping_task() for worker in workers]
+            tasks.append(self.asyncManager.start_all_queues())
+            await asyncio.gather(*tasks)
+
+            # for productName in self.productNames:
+            #     await self.__search_place(productName)
+            #     tasks = [self.asyncManager.start_all_queues(), self.__get_all_products_links()]
+            #     await asyncio.gather(*tasks)
+            #     # await self.__get_all_products_links()
+
         
 
+
 class TravellingScraperWorker(PlaywrightBaseScraper):
-    def __init__(self, main_context: BrowserContext, homepage: str, place: str, timeRange: str):
+    # config_values = read_config()
+    def __init__(self, main_context: BrowserContext, homepage: str, place: str, asyncManager: AsyncioManager, scroll_page_attempt: int):
         self.main_context = main_context
         self.homepage = homepage
         self.place = place
-        self.timeRange = timeRange
+        self.asyncManager = asyncManager
+        self.scroll_page_attempt = scroll_page_attempt
         self.result = []
 
     async def scraping_task(self):
         self.page = await self.initialize_page(self.main_context, self.homepage)
+        # threading.Thread(target=self.__close_dialog, args=(), daemon=True).start()
         await self.__search_place()
         links = await self.__scan_matches()
         for link in links:
-            await self.__scrape_html_source(link)
-        # await self.__iterate_multiple_place_tables()
-        with open(f"scrape_results/transaction/{self.place}.json", "w+", encoding="utf-8") as f:
-            json.dump(self.result, f, ensure_ascii=False, indent=4)
-        # async with aiofiles.open(f"scrape_results/{self.place}_{str(uuid.uuid1())}.json", "w", encoding="utf-8") as f:
-        #     await f.write(json.dumps(self.result, ensure_ascii=False, indent=4))
+            await self.asyncManager.add_task(self.__scrape_html_source(link))
+        
         # print(pd.DataFrame.from_dict(self.result))
         # self.db.submit_placeprice_data(self.place, self.result)
         # self.db.close_connection()
@@ -71,15 +100,15 @@ class TravellingScraperWorker(PlaywrightBaseScraper):
     async def __scan_matches(self):
         load_more_button_selector = "button.de576f5064.b46cd7aad7.d0a01e3d83.dda427e6b5.bbf83acb81.a0ddd706cc"
         matches_list_selector = "//div[@role='list']/div[@role='listitem']/div/div/div/div/div/div/div/div/h3/a"
-        attempts = 5
-        while attempts > 0:
+        while self.scroll_page_attempt > 0:
             await PlaywrightUtils.scroll_to_bottom(self.page, delay=5)
             if await PlaywrightUtils.wait_for_element_no_attempt(self.page, load_more_button_selector, 3000):
                 await self.page.locator(load_more_button_selector).click()
-            attempts -= 1
+            self.scroll_page_attempt -= 1
         matches_list_element = self.page.locator(matches_list_selector)
         links = await matches_list_element.evaluate_all("elements => elements.map(e => e.href)")
         return links
+        
 
     async def __scrape_html_source(self, product_page):
         page = await self.initialize_page(self.main_context, product_page)
@@ -90,7 +119,7 @@ class TravellingScraperWorker(PlaywrightBaseScraper):
         reviews_scorecard_selector = "//div[@id='js--hp-gallery-scorecard']"
         overall_review_selector = "//div[@data-testid='review-score-right-component']/div[contains(@class, 'f63b14ab7a')]"
         review_card_selector = "//div[@data-testid='review-card']/div/div/div[@aria-label='Review']"
-        next_button_selector = "button.de576f5064.b46cd7aad7.e26a59bb37.c295306d66.c7a901b0e7.aaf9b6e287.fe5e267e55"
+        next_button_selector = "//div[contains(@class,'d8026226a7')]/button[@aria-label='Next page']"
 
         place_name = await page.locator(place_name_selector).text_content()
         address = await page.locator(address_selector).text_content()
@@ -108,10 +137,10 @@ class TravellingScraperWorker(PlaywrightBaseScraper):
         except:
             print('No facilities')
         
-        attempts = 1
+        attempts = 5
         comment_result = []
         try:
-            if await PlaywrightUtils.wait_for_element_no_attempt(page, reviews_scorecard_selector, timeout=5000):
+            if await PlaywrightUtils.wait_for_element_no_attempt(page, reviews_scorecard_selector, timeout=15000):
                 await page.locator(reviews_scorecard_selector).click()
                 await asyncio.sleep(3)
                 await page.locator("//div[@role='dialog']/div/div[@class='c1cb99b7ca']").evaluate("e => e.scrollTop += 1200")
@@ -123,20 +152,21 @@ class TravellingScraperWorker(PlaywrightBaseScraper):
                         break
                     for card in await review_cards.all():
                         comment_title_locator: Locator = card.locator("//h4[@data-testid='review-title']")
-                        comment_title = await comment_title_locator.text_content()
+                        comment_title = await comment_title_locator.text_content(timeout=3000)
                         comment_score_locator: Locator = card.locator("//div[@data-testid='review-score']/div/div[@aria-hidden='true']")
-                        comment_score = await comment_score_locator.text_content()
+                        comment_score = await comment_score_locator.text_content(timeout=3000)
                         positive_comment = None
                         negative_comment = None
-                        try:
-                            positive_comment_locator = page.locator("//div[@data-testid='review-positive-text']/div/div[@class='ea9fc823c1']/span")
-                            positive_comment = await positive_comment_locator.text_content()
-                        except:
-                            print('No pos comment')
                         
                         try:
-                            negative_comment_locator = page.locator("//div[@data-testid='review-negative-text']/div/div[@class='ea9fc823c1']/span")
-                            negative_comment = await negative_comment_locator.text_content()
+                            positive_comment_locator = card.locator("//div[@data-testid='review-positive-text']/div/div[@class='ea9fc823c1']/div/span")
+                            positive_comment = await positive_comment_locator.text_content(timeout=3000)
+                        except Exception as e:
+                            print(f'No pos comment: {e}')
+                        
+                        try:
+                            negative_comment_locator = card.locator("//div[@data-testid='review-negative-text']/div/div[@class='ea9fc823c1']/div/span")
+                            negative_comment = await negative_comment_locator.text_content(timeout=3000)
                         except:
                             print('No pos comment')
                         comment_result.append(
@@ -147,10 +177,9 @@ class TravellingScraperWorker(PlaywrightBaseScraper):
                                 "negative_comment": negative_comment
                             }
                         )
-                        print(comment_result)
                     await page.locator(next_button_selector).click()
                     attempts -= 1
-                    await page.locator("//div[@role='dialog']/div/div[@class='c1cb99b7ca']").evaluate("e => e.scrollTop += 100")
+                    await page.locator("//div[@role='dialog']/div/div[@class='c1cb99b7ca']").evaluate("e => e.scrollTop += 1200")
         finally:
             await page.close()
             self.result.append(
@@ -162,5 +191,5 @@ class TravellingScraperWorker(PlaywrightBaseScraper):
                         "comments": comment_result
                     }
                 )
-            print(self.result)
-    
+            async with aiofiles.open(f"scrape_results/{self.place}.json", "w", encoding="utf-8") as f:
+                await f.write(json.dumps(self.result, ensure_ascii=False, indent=4))
